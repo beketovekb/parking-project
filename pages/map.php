e21582f56c6681aef47c5c01d6560abecafe0421
beketovekb <beketovekb@gmail.com>
2025-03-31 03:01:07 +0500

    update2.0.0.1

--- pages/map.php
+++ pages/map.php
@@ -153,11 +153,63 @@ $gmapsKey = "AIzaSyDjB46uCIzzBxbQEhfdsggoXmDybRdlnNA";
       margin: 0 auto; 
       display: block;
     }
+
+    .menu-button {
+      position: absolute;
+      top: 16px;
+      right: 16px;
+      z-index: 20000;
+    }
   </style>
 </head>
 <body>
+  <!-- Кнопка Меню (справа, поверх карты) -->
+  <button 
+    class="btn btn-secondary menu-button" 
+    type="button"
+    data-bs-toggle="offcanvas" 
+    data-bs-target="#menuOffcanvas"
+    aria-controls="menuOffcanvas"
+  >
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+</svg>
+  </button>
 
 <div id="map"></div>
+<!-- Offcanvas Меню -->
+<div 
+    class="offcanvas offcanvas-end" 
+    tabindex="-1" 
+    id="menuOffcanvas" 
+    aria-labelledby="menuOffcanvasLabel"
+  >
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="menuOffcanvasLabel">Меню</h5>
+      <button 
+        type="button" 
+        class="btn-close text-reset" 
+        data-bs-dismiss="offcanvas" 
+        aria-label="Закрыть"
+      ></button>
+    </div>
+    <div class="offcanvas-body">
+      <a href="profile.php" class="btn btn-primary w-100 mb-2">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-badge" viewBox="0 0 16 16">
+  <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492z"/>
+</svg>
+        Личный кабинет
+      </a>
+      <a href="logout.php" class="btn btn-danger w-100">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+  <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+</svg>
+        Выход
+      </a>
+    </div>
+  </div>
 
 <!-- Кнопка GPS -->
 <button class="gps-button" id="gpsBtn">
@@ -562,7 +614,7 @@ function showCurrentStep() {
     return;
   }
   const step = currentSteps[currentStepIndex];
-  const instructionText = step.html_instructions || step.instructions || '';
+  let instructionText = step.html_instructions || step.instructions || '';
   currentStepArea.innerHTML = `
     <div>
       <h5>Шаг ${currentStepIndex+1} из ${currentSteps.length}</h5>
@@ -570,6 +622,24 @@ function showCurrentStep() {
       <small>Расстояние: ${step.distance.text}</small>
     </div>
   `;
+
+  // Удаляем HTML-теги, чтобы не зачитывать <b> и т.п. - если хотите
+  const plainText = instructionText.replace(/<[^>]+>/g, '');
+
+  // Проговариваем
+  speakTTS(plainText);
+}
+/** Функция озвучки (Web Speech API) */
+function speakTTS(text) {
+  if (!text) return;
+  if ('speechSynthesis' in window) {
+    const utter = new SpeechSynthesisUtterance(text);
+    // Можно задать язык (русский)
+    utter.lang = 'ru-RU'; 
+    speechSynthesis.speak(utter);
+  } else {
+    console.warn('Web Speech API не поддерживается в этом браузере');
+  }
 }
 
 /** Проверяем, не отклонился ли пользователь от маршрута */
